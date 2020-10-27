4e76a1b0154fccffbc0d9b168bcead59f24dc3eb
Manashree Jhawar <60222820+manshri99@users.noreply.github.com>
2020-10-27 13:50:33 +0530

    Update form.js

--- temp/form.js
+++ temp/form.js
@@ -11,7 +11,7 @@ $('.email').on("change keyup paste",
 function validateEmail(emailField){
         var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-        if (reg.test(emailField.value) == false) 
+        if (reg.test(emailField.value) == false)
         {
             alert('Invalid Email Address');
             return false;
